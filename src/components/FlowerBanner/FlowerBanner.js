1d5611749c42c057d2108fb082f9b7ce872e1c9d
oscarheimdahl <o.heimdahl@gmail.com>
2022-03-12 21:40:05 +0100

    css stuff

--- src/components/FlowerBanner/FlowerBanner.js
+++ src/components/FlowerBanner/FlowerBanner.js
@@ -1,7 +1,28 @@
 import './flower-banner.scss';
+import flowerBannerImg from 'Images/flower-banner.png';
+
 export function FlowerBanner(rotated) {
+  const img = document.createElement('img');
+  img.src = flowerBannerImg;
+  if (rotated) img.className = 'rotated';
+
+  const img2 = document.createElement('img');
+  img2.src = flowerBannerImg;
+  if (rotated) img2.className = 'rotated';
+
+  const img3 = document.createElement('img');
+  img3.src = flowerBannerImg;
+  if (rotated) img3.className = 'rotated';
+
+  const img4 = document.createElement('img');
+  img4.src = flowerBannerImg;
+  if (rotated) img4.className = 'rotated';
+
   const banner = document.createElement('div');
   banner.id = 'flower-banner';
-  if (rotated) banner.className = 'rotated';
+  banner.appendChild(img);
+  banner.appendChild(img2);
+  banner.appendChild(img3);
+  banner.appendChild(img4);
   return banner;
 }
